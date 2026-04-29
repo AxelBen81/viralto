a6e25bf1c5908753129dac2bf5b4b0a759f383ff
AxelBen81 <axelbenmarnia1381@gmail.com>
2026-04-29 16:02:35 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/how-it-works", "/pricing"],
+});
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
