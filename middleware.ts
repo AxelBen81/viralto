1d6691a20692aea67abafc4c56cac5784685fa51
AxelBen81 <axelbenmarnia1381@gmail.com>
2026-05-07 15:57:02 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({
   publicRoutes: [
     "/",
     "/how-it-works", 
     "/pricing",
     "/api/generations(.*)",
+    "/api/stripe/webhook",
   ],
-  ignoredRoutes: ["/api/generations(.*)"],
+  ignoredRoutes: ["/api/generations(.*)", "/api/stripe/webhook"],
 });
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
